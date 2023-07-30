28877f3db82fe0f47b46cf96dacda0643ed782fb
Noel Welsh <noel@noelwelsh.com>
2023-07-30 17:46:52 +0100

    Incremental progress
    Still doesn't compile

--- generic/shared/src/main/scala/gooey/generic/Generic.scala
+++ generic/shared/src/main/scala/gooey/generic/Generic.scala
@@ -1,22 +1,27 @@
 package gooey.generic
 
 import gooey.component.*
+import gooey.syntax.all.*
 import magnolia1.*
 
+type ToComponentAlgebra = And.Algebra & Text.Algebra & Textbox.Algebra
+
 trait ToComponent[A] {
-  extension (v: A) def toComponent: Component[Textbox.Algebra & And.Algebra, A]
+  extension (v: A) def toComponent: Component[ToComponentAlgebra, A]
 }
 
 object ToComponent extends AutoDerivation[ToComponent] {
-  def join[A](ctx: CaseClass[ToComponent, A]): ToComponent[A] = value =>
-    ctx.params
-      .map { param =>
-        param.typeclass.toComponent(param.deref(value))
-      }
-      .mkString(s"${ctx.typeInfo.short}(", ",", ")")
+  def join[A](ctx: CaseClass[ToComponent, A]): ToComponent[A] =
+    value =>
+      ctx.params
+        .map { param =>
+          param.typeclass.toComponent(param.deref(value))
+        }
+        .fold(_ and _)
 
   override def split[A](ctx: SealedTrait[ToComponent, A]): ToComponent[A] =
-    value => ctx.choose(value) { sub => sub.typeclass.print(sub.cast(value)) }
+    value =>
+      ctx.choose(value) { sub => sub.typeclass.toComponent(sub.cast(value)) }
 
   given ToComponent[String] = s => Textbox.empty.withDefault(s)
 }
