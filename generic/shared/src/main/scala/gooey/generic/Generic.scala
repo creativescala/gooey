e40f7b8fa2e9674128780c407c6e0e41725ec538
Noel Welsh <noel@noelwelsh.com>
2023-08-01 13:47:39 +0100

    Basic working generic derivation

--- generic/shared/src/main/scala/gooey/generic/Generic.scala
+++ generic/shared/src/main/scala/gooey/generic/Generic.scala
@@ -1,27 +1,56 @@
+/*
+ * Copyright 2023 Creative Scala
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package gooey.generic
 
 import gooey.component.*
 import gooey.syntax.all.*
-import magnolia1.*
 
-type ToComponentAlgebra = And.Algebra & Text.Algebra & Textbox.Algebra
+import scala.compiletime.*
+import scala.deriving.*
+
+type ToComponentAlgebra = And.Algebra & Map.Algebra & Pure.Algebra &
+  Text.Algebra & Textbox.Algebra
 
 trait ToComponent[A] {
-  extension (v: A) def toComponent: Component[ToComponentAlgebra, A]
+  def toComponent: Component[ToComponentAlgebra, A]
 }
 
-object ToComponent extends AutoDerivation[ToComponent] {
-  def join[A](ctx: CaseClass[ToComponent, A]): ToComponent[A] =
-    value =>
-      ctx.params
-        .map { param =>
-          param.typeclass.toComponent(param.deref(value))
-        }
-        .fold(_ and _)
+object ToComponent {
+  inline def derived[A](using m: Mirror.Of[A]): ToComponent[A] = {
+    inline m match {
+      case s: Mirror.SumOf[A]     => ???
+      case p: Mirror.ProductOf[A] => fromProduct(p)
+    }
+  }
 
-  override def split[A](ctx: SealedTrait[ToComponent, A]): ToComponent[A] =
-    value =>
-      ctx.choose(value) { sub => sub.typeclass.toComponent(sub.cast(value)) }
-
-  given ToComponent[String] = s => Textbox.empty.withDefault(s)
+  inline def fromProduct[A](m: Mirror.ProductOf[A]): ToComponent[A] = {
+    new ToComponent[A] {
+      def toComponent: Component[ToComponentAlgebra, A] = {
+        type ElemToComponent = Tuple.Map[m.MirroredElemTypes, ToComponent]
+        val elemToComponents: ElemToComponent = summonAll[ElemToComponent]
+        val component: Component[ToComponentAlgebra, A] =
+          elemToComponents.toList
+            .asInstanceOf[List[Component[ToComponentAlgebra, Any]]]
+            .foldLeft(Pure[Tuple](EmptyTuple).widen[ToComponentAlgebra])(
+              (accum, elt) => accum.and(elt).map((l, r) => l :* r)
+            )
+            .map(tuple => m.fromProduct(tuple))
+        component
+      }
+    }
+  }
 }
