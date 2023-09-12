9fd76681f3e37ca155c43f569cd9f2c2749849d7
Noel Welsh <noel@noelwelsh.com>
2023-09-12 09:43:18 +0100

    gooey.all import
    Import everything useful in one go.
    
    Closes #12

---
+++ core/shared/src/main/scala/gooey/all.scala
@@ -0,0 +1,25 @@
+package gooey
+
+object all {
+  export gooey.{Display, Observable, Var}
+  export gooey.component.{
+    Above,
+    And,
+    Beside,
+    Checkbox,
+    Component,
+    Displayable,
+    Dropdown,
+    Form,
+    Labelable,
+    Map,
+    Observe,
+    Observed,
+    Pure,
+    Slider,
+    Styleable,
+    Text,
+    Textbox
+  }
+  export gooey.syntax.all.*
+}
